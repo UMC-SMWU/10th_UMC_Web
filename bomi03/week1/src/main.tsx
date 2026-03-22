94eb605d74bc9b77730c84429903a24f825a1b98
bomi03 <kbm03@sookmyung.ac.kr>
2026-03-23 03:19:35 +0900

    [Feat(보리/김보미)] 1주차 미션 1 완료

--- bomi03/week1/src/main.tsx
+++ bomi03/week1/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import "./practice/type"
+// import "./practice/type" // 핵심 키워드 실습 파일
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
