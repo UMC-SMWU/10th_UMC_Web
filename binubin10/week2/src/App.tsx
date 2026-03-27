89df0a0a21492a59fb523b09403000c447d6fca3
dbstjqls <130419093+dbstjqls@users.noreply.github.com>
2026-03-27 16:35:47 +0900

    [Test(피노/윤서빈)] 2주차 실습 코드

--- binubin10/week2/src/App.tsx
+++ binubin10/week2/src/App.tsx
@@ -1,27 +1,14 @@
-import './App.css';
-// 1) List 컴포넌트를 import 해요.
-import List from './components/List';
+import ButtonGroup from './components/ButtonGroup';
+import { useCount } from './context/CounterProvider';
 
 function App() {
-  const nickname = '피노';
-  const sweetPotato = '고구마';
-  const array = ['REACT', 'NEXT', 'VUE', 'SVELTE', 'ANGULAR', 'REACT-NATIVE'];
+  // const context = useContext(CounterContext);
+  const { count } = useCount();
 
   return (
     <>
-      <strong className="school">숙명여자대학교</strong>
-      <p
-        className="user-nickname"
-        style={{ color: 'purple', fontWeight: 'bold', fontSize: '3rem' }}
-      >
-        {nickname}/윤서빈
-      </p>
-      <h1 className="main-sentence">{`${nickname}는 ${sweetPotato} 아이스크림을 좋아합니다.`}</h1>
-      <ul className="stack-list">
-        {array.map((yaho, idx) => (
-          <List key={idx} tech={yaho} />
-        ))}
-      </ul>
+      <h1>{count}</h1>
+      <ButtonGroup />
     </>
   );
 }
