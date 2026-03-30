89a53b460fe8252d71bacc863d2fbabfc44a0eaa
bomi03 <kbm03@sookmyung.ac.kr>
2026-03-30 20:03:37 +0900

    [Feat(보리/김보미)] 폴더 구조 변경

---
+++ bomi03/week2/mission1/src/components/TodoForm.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { useTodo } from "../context/TodoContext";
+
+interface TodoFormProps {
+  input: string;
+  setInput: (input: string) => void;
+  handleSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
+}
+
+const TodoForm = () => {
+   const {addTodo} = useTodo();
+   const [input, setInput] = useState<string>('');
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) : void => {
+        e.preventDefault();
+        const text = input.trim();
+
+        if (text) {
+            addTodo(text);
+            setInput('');
+        }
+    };
+
+  return (
+    <form onSubmit={handleSubmit} 
+    className='todo-container__form'>
+                <input 
+                  value={input} 
+                  onChange={(e) => setInput(e.target.value)} 
+                  type='text' className='todo-container__input' 
+                  placeholder='할 일 입력' 
+                  required/>
+                <button 
+                  type='submit' 
+                  className='todo-container__button'>
+                    할 일 추가
+                  </button>
+            </form>
+  );
+};
+
+export default TodoForm;
