b45a2c853395ab807a8893f3f1ecadb319cf923c
bomi03 <kbm03@sookmyung.ac.kr>
2026-03-30 21:35:10 +0900

    [Feat(보리/김보미)] 미션2 완료

---
+++ bomi03/week2/mission2/src/ThemeContent.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { THEME, useTheme } from './context/ThemeProvider';
+import clsx from 'clsx';
+
+const ThemeContent = () => {
+
+    const {theme, toggleTheme } = useTheme();
+  
+    const isLightMode = theme === THEME.LIGHT;
+
+  return (
+    <div className={clsx('p-4 h-dvh w-full', isLightMode ? 'bg-white' : 'bg-gray-800')}
+    >
+      <h1
+        className={clsx(
+          'text-wxl font-bold',
+          isLightMode ? 'text-black' : 'text-white'
+        )}
+      >
+        Theme Content
+      </h1>
+      <p className={clsx('mt-2', isLightMode ? 'text-black' : 'text-white')}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero modi nisi ex ad facilis aperiam possimus minima praesentium repellendus odio veritatis dolores, suscipit aut optio officiis voluptatibus expedita laudantium molestiae!
+      </p>
+    </div>
+  )
+}
+
+export default ThemeContent
