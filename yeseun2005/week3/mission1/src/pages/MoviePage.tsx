f8b93a71fbc494150dd79f0c1fff820aabc21b15
yeseun2005 <yeseun2005@sookmyung.ac.kr>
2026-04-08 01:03:57 +0900

    [Feat(차미/이예은)] 페이지네이션 버튼 및 로딩 스피너 분기 처리 추가

--- yeseun2005/week3/mission1/src/pages/MoviePage.tsx
+++ yeseun2005/week3/mission1/src/pages/MoviePage.tsx
@@ -51,16 +51,16 @@ export default function MoviePage() {
         <>
         <div className='flex items-center justify-center gap-6 mt-5'>
             <button 
-            className='bg-[#dda5e3] text-white px-6 py-3 rounded-lg shadow-md
-            hover:bg-[#b2dab1] transition-all duration-200 disabled:bg-gray-300
+            className='bg-[#0080ff] text-white px-6 py-3 rounded-lg shadow-md
+            hover:bg-[#344580] transition-all duration-200 disabled:bg-gray-300
             cursor-pointer disabled:cursor-not-allowed'
             disabled={page === 1}
             onClick={() => setPage((prev) => prev - 1)}
             >{`<`}</button>
             <span>{page} 페이지</span>
             <button
-            className='bg-[#dda5e3] text-white px-6 py-3 rounded-lg shadow-md
-            hover:bg-[#b2dab1] transition-all duration-200 cursor-pointer'
+            className='bg-[#0080ff] text-white px-6 py-3 rounded-lg shadow-md
+            hover:bg-[#344580] transition-all duration-200 cursor-pointer'
             onClick={() => setPage((prev) => prev +1)}
             >{`>`}</button>
         </div>
