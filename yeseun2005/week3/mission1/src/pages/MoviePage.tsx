04fb875f296c45afecab95f958d19c3b297d53f3
yeseun2005 <yeseun2005@sookmyung.ac.kr>
2026-04-07 23:41:01 +0900

    [Feat(차미/이예은)] pagination 기능 추가

--- yeseun2005/week3/mission1/src/pages/MoviePage.tsx
+++ yeseun2005/week3/mission1/src/pages/MoviePage.tsx
@@ -2,38 +2,62 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import type { Movie, MovieResponse } from '../types/movie';
 import MovieCard from '../components/MovieCard';
+import { LoadingSpinner } from './LoadingSpinner';
 
 export default function MoviePage() {
     const [movies, setMovies] = useState<Movie[]>([]);
+    /// 1. 로딩 상태
+    const [isPending, setIsPending] = useState(false);
+    /// 2. 에러 상태
+    const [isError, setIsError] = useState(false);
+    /// 3. 페이지
+    const [page, setPage] = useState(1);
 
     useEffect(() => {
         const fetchMovies = async () => {
+            setIsPending(true);
+
+            try {
             const { data } = await axios.get<MovieResponse>(
-                 `https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=1`,
+                 `https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=$
+                 {page}`,
                  {
                     headers : {
                         Authorization: `Bearer ${import.meta.env.VITE_TMDB_KEY}`,
                     },
                  }
+                
             );
 
-            console.log(data);
 
             setMovies(data.results);
-
-        };
+            setIsPending(false);
+        } catch {
+            setIsError(true);
+        } finally {
+            setIsPending(false);
+        }
+    };
             
         fetchMovies();
     }, []);
 
-    
+    if (!isError) {
+        return (
+            <div>
+                <span className='text-red-500 text-2xl'>에러가 발생했습니다.</span>
+            </div>
+        );
+    }
+
     return (
-    <div className='p-10 grid gap-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-4
+        <>
+        <div className='p-10 grid gap-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-4
     lg:grid-cols-5 xl:grid-cols-6'>
         {movies.map((movie) => (
             <MovieCard key={movie.id} movie={movie} />
         ))}
     </div>
-
+        </>
     );
 }
