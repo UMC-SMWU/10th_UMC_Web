64a2ab2f44048e321ee9e475b20c32758236f65b
yeseun2005 <yeseun2005@sookmyung.ac.kr>
2026-04-08 04:34:12 +0900

    [Fix(차미/이예은)] movieID / movieId 대소문자 불일치로 인한 params 오류 수정

--- yeseun2005/week3/mission1/src/App.tsx
+++ yeseun2005/week3/mission1/src/App.tsx
@@ -3,6 +3,7 @@ import HomePage from './pages/HomePage';
 import MoviePage from './pages/MoviePage';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import NotFoundPage from './pages/NotFoundPage';
+import MovieDetailPage from './pages/MovieDetailPage';
 
 const router = createBrowserRouter([
   {
@@ -14,6 +15,10 @@ const router = createBrowserRouter([
         path: 'movies/:category',
         element: <MoviePage />,
       },
+      {
+        path: 'movie/:movieID',
+        element: <MovieDetailPage />
+      }
     ],
   },
 ]);
