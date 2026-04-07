44ac7e02bda03c2eab1d9f0ed0cd3f68834361e7
yeseun2005 <yeseun2005@sookmyung.ac.kr>
2026-04-08 00:58:21 +0900

    [Fix(차미/이예은)] pagination 오류 수정

--- yeseun2005/week3/mission1/src/pages/MoviePage.tsx
+++ yeseun2005/week3/mission1/src/pages/MoviePage.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import type { Movie, MovieResponse } from '../types/movie';
 import MovieCard from '../components/MovieCard';
-import { LoadingSpinner } from './LoadingSpinner';
+import { LoadingSpinner } from '../components/LoadingSpinner';
 
 export default function MoviePage() {
     const [movies, setMovies] = useState<Movie[]>([]);
@@ -16,22 +16,19 @@ export default function MoviePage() {
     useEffect(() => {
         const fetchMovies = async () => {
             setIsPending(true);
+            setIsError(false);
 
             try {
             const { data } = await axios.get<MovieResponse>(
-                 `https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=$
-                 {page}`,
+                 `https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=${page}`,
                  {
                     headers : {
                         Authorization: `Bearer ${import.meta.env.VITE_TMDB_KEY}`,
                     },
                  }
-                
             );
 
-
             setMovies(data.results);
-            setIsPending(false);
         } catch {
             setIsError(true);
         } finally {
@@ -40,9 +37,9 @@ export default function MoviePage() {
     };
             
         fetchMovies();
-    }, []);
+    }, [page]);
 
-    if (!isError) {
+    if (isError) {
         return (
             <div>
                 <span className='text-red-500 text-2xl'>에러가 발생했습니다.</span>
@@ -52,12 +49,35 @@ export default function MoviePage() {
 
     return (
         <>
-        <div className='p-10 grid gap-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-4
-    lg:grid-cols-5 xl:grid-cols-6'>
-        {movies.map((movie) => (
-            <MovieCard key={movie.id} movie={movie} />
-        ))}
-    </div>
+        <div className='flex items-center justify-center gap-6 mt-5'>
+            <button 
+            className='bg-[#dda5e3] text-white px-6 py-3 rounded-lg shadow-md
+            hover:bg-[#b2dab1] transition-all duration-200 disabled:bg-gray-300
+            cursor-pointer disabled:cursor-not-allowed'
+            disabled={page === 1}
+            onClick={() => setPage((prev) => prev - 1)}
+            >{`<`}</button>
+            <span>{page} 페이지</span>
+            <button
+            className='bg-[#dda5e3] text-white px-6 py-3 rounded-lg shadow-md
+            hover:bg-[#b2dab1] transition-all duration-200 cursor-pointer'
+            onClick={() => setPage((prev) => prev +1)}
+            >{`>`}</button>
+        </div>
+        {isPending && (
+            <div className='flex items-center justify-center h-dvh'>
+                <LoadingSpinner />
+            </div>
+        )}
+
+        {!isPending && (
+            <div className='p-10 grid gap-4 grid-cols-2 sm:grid-cols-3 
+            md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6'>
+                {movies.map((movie) => (
+                    <MovieCard key={movie.id} movie={movie} />
+                ))}
+            </div>
+        )}
         </>
     );
 }
