c86e55e933e236732e3a045facd175ca91b67b39
yeseun2005 <yeseun2005@sookmyung.ac.kr>
2026-04-07 21:10:34 +0900

    [Feat(차미/이예은)] week3 mission1 완료

--- yeseun2005/week3/mission1/src/components/MovieCard.tsx
+++ yeseun2005/week3/mission1/src/components/MovieCard.tsx
@@ -1,14 +1,37 @@
 import type { Movie } from '../types/movie';
+import { useState } from 'react';
 
 interface MovieCardProps {
     movie: Movie;
 }
 
 export default function MovieCard({movie} : MovieCardProps) {
-    console.log(movie.poster_path);
+    const [isHovered, setIsHovered] = useState(false);
+
     return (
-        <div>
-            <img src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} />
+        <div
+        className='relative rounded-xl shadow-lg overflow-hidden cursor-pointer
+        w-44 transition-transform duration-500 hover:scale-105'
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+        >
+            <img 
+            src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+            alt={`${movie.title} 영화의 이미지`}
+            className='' 
+            />
+
+            {isHovered && (
+                <div className='absolute inset-0 bg-linear-to-t from-black/50
+                to-transparent backdrop-blur-md flex flex-col justify-center
+                items-center text-white p-4'>
+                    <h2 className='text-lg font-bold leading-snug'>(movie.title)</h2>
+                    <p className='text-sm text-gray-300 leading-relaxed mt-2
+                    line-clam-5'>
+                        {movie.overview}
+                    </p>
+                </div>
+            )}
         </div>
     );
 }
--- yeseun2005/week3/mission1/src/pages/MoviePage.tsx
+++ yeseun2005/week3/mission1/src/pages/MoviePage.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import type { Movie } from '../types/movie';
+import type { Movie, MovieResponse } from '../types/movie';
 import MovieCard from '../components/MovieCard';
 
 export default function MoviePage() {
@@ -8,8 +8,8 @@ export default function MoviePage() {
 
     useEffect(() => {
         const fetchMovies = async () => {
-            const { data = [] } = await axios(
-                 `https://api.themoviedb.org/3/movie/popular?language=en-US&page=1`,
+            const { data } = await axios.get<MovieResponse>(
+                 `https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=1`,
                  {
                     headers : {
                         Authorization: `Bearer ${import.meta.env.VITE_TMDB_KEY}`,
@@ -17,7 +17,10 @@ export default function MoviePage() {
                  }
             );
 
+            console.log(data);
+
             setMovies(data.results);
+
         };
             
         fetchMovies();
@@ -25,7 +28,8 @@ export default function MoviePage() {
 
     
     return (
-    <div>
+    <div className='p-10 grid gap-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-4
+    lg:grid-cols-5 xl:grid-cols-6'>
         {movies.map((movie) => (
             <MovieCard key={movie.id} movie={movie} />
         ))}
