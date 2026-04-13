65232a6f95ec0836e38b5fd6b915d7e34a4f0443
dbstjqls <130419093+dbstjqls@users.noreply.github.com>
2026-04-13 14:50:55 +0900

    [Feat(피노/윤서빈)] mission3 완료

---
+++ binubin10/week4/mission3/src/apis/axios.ts
@@ -0,0 +1,17 @@
+import axios from 'axios';
+import { LOCAL_STORAGE_KEY } from '../constants/key';
+
+export const axiosInstance = axios.create({
+  baseURL: 'http://localhost:8000',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem(LOCAL_STORAGE_KEY.accessToken);
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
