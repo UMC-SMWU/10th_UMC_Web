c6437355f8bbb6c54b1a3b43ee39b5e6e87f4451
yw11191 <jyeono3c@gmail.com>
2026-05-04 16:45:15 +0900

    [주연우/우주] 5주차 mission1 정리내용 제출.

---
+++ yw11191/week4/mission1/src/App.tsx
@@ -0,0 +1,48 @@
+import { createBrowserRouter, RouterProvider, type RouteObject } from "react-router-dom";
+import "./App.css";
+import HomePage from "./pages/HomePage";
+import NotFoundPage from "./pages/NotFoundPage";
+import LoginPage from "./pages/LoginPage";
+import HomeLayout from "./layouts/HomeLayout";
+import SignupPage from "./pages/SignupPage";
+import MyPage from "./pages/MyPage";
+import { AuthProvider } from "./context/AuthContext";
+import ProtectedLayout from "./layouts/ProtectedLayout";
+
+const publicRoutes: RouteObject[] = [
+  {
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <NotFoundPage />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "login", element: <LoginPage /> },
+      { path: "signup", element: <SignupPage /> },
+    ],
+  },
+];
+
+const protectedRoutes: RouteObject[] = [
+  {
+    path: "/",
+    element: <ProtectedLayout />,
+    errorElement: <NotFoundPage />,
+    children: [
+      {
+        path: "mypage",
+        element: <MyPage />,
+      },
+    ],
+  },
+];
+const router = createBrowserRouter([...publicRoutes, ...protectedRoutes]);
+
+function App() {
+  return (
+    <AuthProvider>
+      <RouterProvider router={router}></RouterProvider>
+    </AuthProvider>
+  );
+}
+
+export default App;
