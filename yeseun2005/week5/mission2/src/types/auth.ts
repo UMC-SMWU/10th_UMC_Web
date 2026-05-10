c607b6fce7b920de78dffa88212fbafff4326390
yeseun2005 <yeseun2005@sookmyung.ac.kr>
2026-05-11 00:20:50 +0900

    [Feat(차미/이예은)] Refresh Token을 활용하여 지속적인 로그인 유지 기능 구현

---
+++ yeseun2005/week5/mission2/src/types/auth.ts
@@ -0,0 +1,43 @@
+import type { CommonRespone } from "./common";
+//회원 가입
+export type RequestSignupDto = {
+    name: string;
+    email: string;
+    bio?: string;
+    avatar?:string;
+    password:string;
+}
+
+export type ResponseSignupDto = CommonRespone<{
+    id:number;
+    name: string;
+    email: string;
+    bio: string| null;
+    createdAt: Date;
+    updateAt: Date;
+}>; 
+
+//로그인
+export type RequestSigninDto = {
+    email: string;
+    password: string;
+};
+
+export type ResponseSigninDto = CommonRespone<{
+    id: number;
+    name: string;
+    accessToken: string;
+    refreshToken: string;
+}>;
+
+//내 정보 조회
+
+export type ResponseMyInfoDto = CommonRespone<{
+    avatar: string;
+    id:number;
+    name: string;
+    email: string;
+    bio: string| null;
+    createdAt: Date;
+    updateAt: Date;
+}>
