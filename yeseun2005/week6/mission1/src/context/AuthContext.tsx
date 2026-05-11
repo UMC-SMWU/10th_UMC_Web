809265ce3ad57a05e766375707b155d2902da217
yeseun2005 <yeseun2005@sookmyung.ac.kr>
2026-05-11 15:26:08 +0900

    [Fix(차미/이예은)] 로그인 오류 수정

---
+++ yeseun2005/week6/mission1/src/context/AuthContext.tsx
@@ -0,0 +1,95 @@
+import { postLogout, postSignin } from "../apis/auth.ts";
+import { LOCAL_STORAGE_KEY } from "../constants/key.ts";
+import { useLocalStorage } from "../hooks/useLocalStorage.ts";
+import type { RequestSigninDto } from "../types/auth.ts";
+import { createContext, useContext, useState, type PropsWithChildren } from "react";
+
+
+interface AuthContextType { 
+    accessToken: string | null;
+    refreshToken: string | null;
+    login: (signInData: RequestSigninDto) => Promise<void>;
+    logout: () => Promise<void>;
+}
+
+export const AuthContext = createContext<AuthContextType>({ 
+    accessToken: null,
+    refreshToken: null,
+    login: async () => {},
+    logout: async () => {},
+});
+
+export const AuthProvider = ({children}:PropsWithChildren) => {
+    const {
+        getItem: getAccessTokenFromStorage,
+        setItem: setAccessTokenInStorage,
+        removeItem: removeAccessTokenFromStorage,
+    } = useLocalStorage(LOCAL_STORAGE_KEY.accessToken);
+    const {
+        getItem: getRefreshTokenFromStorage,
+        setItem: setRefreshTokenInStorage,
+        removeItem: removeRefreshTokenFromStorage,
+    } = useLocalStorage(LOCAL_STORAGE_KEY.refreshToken);
+
+    const [accessToken, setAccessToken] = useState<string|null>(
+        getAccessTokenFromStorage(),
+    );
+
+    const [refreshToken, setRefreshToken] = useState<string | null>(
+        getRefreshTokenFromStorage(),
+    );
+
+    const login = async (signInData: RequestSigninDto) => {
+        try {
+            const { data } = await postSignin(signInData);
+
+        if (data) {
+            const newAccessToken = data.accessToken;
+            const newRefreshToken = data.refreshToken;
+
+            setAccessTokenInStorage(newAccessToken);
+            setRefreshTokenInStorage(newRefreshToken);
+
+            setAccessToken(newAccessToken);
+            setRefreshToken(newRefreshToken);
+            alert("로그인 성공");
+            window.location.href = "/my";
+        }
+        }catch(error) {
+            console.error("로그인 오류", error);
+            alert("로그인 실패");
+        }
+        
+    };
+
+    const logout = async() => {
+        try {
+            await postLogout();
+            removeAccessTokenFromStorage();
+            removeRefreshTokenFromStorage();
+
+            setAccessToken(null);
+            setRefreshToken(null);
+
+            alert("로그아웃 성공")
+        } catch (error){
+            console.error("로그아웃 오류", error);
+            alert("로그아웃 실패");
+        }
+    };
+
+    return (
+        <AuthContext.Provider value={{accessToken, refreshToken, login, logout}}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export const useAuth = () => {
+    const context: AuthContextType = useContext(AuthContext);
+    if (!context) {
+        throw new Error("AuthContext를 찾을 수 없습니다.");
+    }
+
+    return context;
+};
