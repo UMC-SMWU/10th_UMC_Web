20f4ff2e5090d877a615b5147827c5fb5134a211
yw11191 <jyeono3c@gmail.com>
2026-05-12 14:10:53 +0900

    [주연우/우주] 6주차 mission1 정리내용 제출.

--- yw11191/week4/mission1/src/pages/HomePage.tsx
+++ yw11191/week4/mission1/src/pages/HomePage.tsx
@@ -1,7 +1,24 @@
+import { useState } from "react";
+import useGetLpList from "../hooks/queries/useGetLpList";
+
 const HomePage = () => {
+    const [search, setSearch]=useState("타입");
+    const {data, isPending, isError} = useGetLpList({
+        search,
+    });
+
+    if (isPending) {
+        return <div className="mt-20">로딩중...</div>
+    }
+
+    if (isError) {
+        return <div className="mt-20">에러가 발생했습니다.</div>
+    }
+
     return (
-        <div>
-            HomePage
+        <div className="mt-20">
+            <input value={search} onChange={(e) => setSearch(e.target.value)} />
+            {data.map((lp) => <h1 key={lp.id}>{lp.title}</h1>)}
         </div>
     )
 };
